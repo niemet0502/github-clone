e24a0ff9cfa2656d3f2828e7ec95999e05e99401
niemet0502 <vincentmarius8@gmail.com>
2021-01-30 16:54:09 +0100

    main :: imported navbar icon from material

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./Navbar.css"
+import GitHubIcon from '@material-ui/icons/GitHub';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import AddIcon from '@material-ui/icons/Add';
 function Navbar() {
   return (
     <div className="Navbar">
-      I'm Navbar
+      <div className="Navbar_items_container">
+        <GitHubIcon className="Navbar__GithubIcon" />
+        <input type="text" placeholder=""/>
+        I'm Navbar
+      </div>
+      <div className="Navbar_icons_container">
+        <NotificationsNoneIcon />
+        <AddIcon />
+      </div>
     </div>
   )
 }
