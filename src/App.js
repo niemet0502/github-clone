c0d4dce609c4e08ca515cc672eb77533599fde06
niemet0502 <vincentmarius8@gmail.com>
2021-01-30 19:34:05 +0100

    main :: imported mode toggle in App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar/Navbar"
 import './App.css';
-
+import ModeToggle from "./components/ModeToogle/ModeToggle"
+import "react-toggle/style.css";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ModeToggle />
     </div>
   );
 }
