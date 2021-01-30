a5d95c016ee451d52ff20d17a35f9048e6f9dd15
niemet0502 <vincentmarius8@gmail.com>
2021-01-30 17:15:15 +0100

    main :: used NavbarItem component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import "./Navbar.css"
 import GitHubIcon from '@material-ui/icons/GitHub';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import AddIcon from '@material-ui/icons/Add';
+import NavbarItem from "../NavbarItem/NavbarItem"
 function Navbar() {
   return (
     <div className="Navbar">
@@ -10,7 +11,12 @@ function Navbar() {
         <GitHubIcon className="Navbar__GithubIcon" />
         <input type="text" placeholder="Search or jump to..." className="header_search_input" />
         <img src="https://github.githubassets.com/images/search-key-slash.svg" className="github_svg" alt=""/>
-       
+        <div className="NavbarItem__container">
+          <NavbarItem title="Pull requests" />
+          <NavbarItem title="Issues" />
+          <NavbarItem title="Marketplace" />
+          <NavbarItem title="Explore" />
+        </div>
       </div>
       <div className="Navbar_icons_container">
         <NotificationsNoneIcon />
