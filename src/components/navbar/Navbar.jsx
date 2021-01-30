24bb7c8a2199d3a0123f9933d07d7247fb4cff6d
niemet0502 <vincentmarius8@gmail.com>
2021-01-30 17:42:28 +0100

    main :: finished navbar mobil version

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,26 +7,33 @@ import NavbarItem from "../NavbarItem/NavbarItem"
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import profil from "../../assets/images/profil.jpg"
 
+import MenuIcon from '@material-ui/icons/Menu';
+
 function Navbar() {
   return (
     <div className="Navbar">
+      <MenuIcon className="toggle_icon" />
       <div className="Navbar_items_container">
         <GitHubIcon className="Navbar__GithubIcon" />
-        <input type="text" placeholder="Search or jump to..." className="header_search_input" />
-        <img src="https://github.githubassets.com/images/search-key-slash.svg" className="github_svg" alt=""/>
-        <div className="NavbarItem__container">
-          <NavbarItem title="Pull requests" />
-          <NavbarItem title="Issues" />
-          <NavbarItem title="Marketplace" />
-          <NavbarItem title="Explore" />
+        <div className="large_menu_container">
+          <input type="text" placeholder="Search or jump to..." className="header_search_input" />
+          <img src="https://github.githubassets.com/images/search-key-slash.svg" className="github_svg" alt=""/>
+          <div className="NavbarItem__container">
+            <NavbarItem title="Pull requests" />
+            <NavbarItem title="Issues" />
+            <NavbarItem title="Marketplace" />
+            <NavbarItem title="Explore" />
+          </div>
         </div>
       </div>
       <div className="Navbar_icons_container">
         <NotificationsNoneIcon className="Right_Icon" />
-        <AddIcon className="Right_Icon" />
-        <ArrowDropDownIcon className=" ArrowIcon" />
-        <img src="https://avatars.githubusercontent.com/u/62050249?s=460&u=0e0a1c398dac0b29e7283b2626b7f5b71fbddd0f&v=4" alt="" className="header_profil"/>
-        <ArrowDropDownIcon className=" ArrowIcon" />
+        <div className="large_menu_container">
+          <AddIcon className="Right_Icon" />
+          <ArrowDropDownIcon className=" ArrowIcon" />
+          <img src="https://avatars.githubusercontent.com/u/62050249?s=460&u=0e0a1c398dac0b29e7283b2626b7f5b71fbddd0f&v=4" alt="" className="header_profil"/>
+          <ArrowDropDownIcon className=" ArrowIcon" />
+        </div>
       </div>
     </div>
   )
